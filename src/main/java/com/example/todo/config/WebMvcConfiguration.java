2eb43e5eabacf1b2a30f2e34184f207e684408a1
takumi0826 <takumi.s8.26@gmail.com>
2021-12-30 21:52:10 +0900

    cors

--- src/main/java/com/example/todo/config/WebMvcConfiguration.java
+++ src/main/java/com/example/todo/config/WebMvcConfiguration.java
@@ -29,7 +29,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT",
+            "DELETE");
       }
     };
   }
