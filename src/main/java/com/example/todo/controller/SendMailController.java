0ff9616702888ecad7e22b9e55be0a9531996277
takumi0826 <takumi.s8.26@gmail.com>
2022-01-20 00:20:02 +0900

    メール送信テスト

--- src/main/java/com/example/todo/controller/SendMailController.java
+++ src/main/java/com/example/todo/controller/SendMailController.java
@@ -2,15 +2,31 @@ package com.example.todo.controller;
 
 import com.example.todo.constant.UrlConst;
 
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class SendMailController {
+
+  private final MailSender mailSender;
 
   @PostMapping(value = UrlConst.MAIL)
   public void init(Model model) {
     System.out.println("sendMail");
+    SimpleMailMessage msg = new SimpleMailMessage();
+    // msg.setTo("");
+    // msg.setCc("送信先メールアドレス2", "送信先メールアドレス3");
+    // msg.setBcc("送信先メールアドレス4");
+    msg.setSubject("件名: テストだよ");
+    msg.setText("本文: テストだよ");
+
+    // メール送信
+    mailSender.send(msg);
   }
 }
