4f68783e0ad4ab8f13d7ec8dbe70eb0a1e281944
takumi0826 <takumi.s8.26@gmail.com>
2022-01-21 23:59:51 +0900

    handler

---
+++ src/main/java/com/example/todo/error/ExceptionGrobalHandler.java
@@ -0,0 +1,17 @@
+package com.example.todo.error;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class ExceptionGrobalHandler {
+  @ExceptionHandler(Exception.class)
+  public String handleException(Exception e, HttpServletResponse response, Model model) {
+    response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    model.addAttribute("errorMessage", e.getMessage());
+
+    return "foo/error";
+  }
+}
