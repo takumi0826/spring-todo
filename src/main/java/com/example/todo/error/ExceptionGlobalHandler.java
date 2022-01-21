a1d6f41cc190eef3ffb030d6d1496583da7e1fb7
takumi0826 <takumi.s8.26@gmail.com>
2022-01-22 00:00:51 +0900

    handler

--- src/main/java/com/example/todo/error/ExceptionGlobalHandler.java
+++ src/main/java/com/example/todo/error/ExceptionGlobalHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-public class ExceptionGrobalHandler {
+public class ExceptionGlobalHandler {
   @ExceptionHandler(Exception.class)
   public String handleException(Exception e, HttpServletResponse response, Model model) {
     response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
